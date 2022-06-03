af49ce434f457eeb9bb2c5a0278fe3e1aa0b95d0
Your Name <you@example.com>
2022-06-03 06:52:03 +0100

    u1

--- init.ps1
+++ init.ps1
@@ -3,3 +3,7 @@ Import-Module ServerManager  -Force;
 Get-WindowsFeature | more
 Get-WindowsFeature *web* | Where-Object {$_.installState -eq 'Installed'}
 Remove-WindowsFeature -WhatIf
+
+Install-WindowsFeature web-server -IncludeAllSubFeature -IncludeManagementTools
+
+need continue on turo 7
