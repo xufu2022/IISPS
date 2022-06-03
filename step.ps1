03fd06b76d0059f14f3f418a860d5eefba9800a3
Your Name <you@example.com>
2022-06-03 16:27:00 +0100

    create web site

--- step.ps1
+++ step.ps1
@@ -5,3 +5,11 @@ Get-Help Find-Module -Examples
 
 # Alias
 Get-Alias -Definition Where-Object
+
+#Create web site
+Import-Module webadministration
+New-Item iis:\sites\website1
+-bindings @{protocol="http";bindingInformation=":80:www.website1.com"}
+-physicalPath %systemdrive%\inetpub\wwwroot\website1
+-applicationPool "DefaultAppPool"
+-WhatIf
